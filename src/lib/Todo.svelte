fb9d5c07b511060597650493c48cd6e85d283866
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-05-29 01:30:50 +0530

    basic todo rendering

---
+++ src/lib/Todo.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let todo;
+</script>
+
+<div class="todo">
+  <h1>{todo.name}</h1>
+  <h3>{todo.id}</h3>
+  <h3>{todo.priority}</h3>
+</div>
+
+<style>
+.todo {
+  border: 5px solid pink;
+  border-radius: 40px;
+  padding: 2px;
+  margin: 69px;
+}
+
+.todo h1 {
+  color: red;
+}
+
+.todo h3 {
+  color: green;
+}
+</style>
